68c1733085ae3ef40e52caa21857047905080053
taxflowmd-ai <taxflowmd@gmail.com>
2026-03-02 12:50:03 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
+    //serverActions: {
+      //allowedOrigins: ['localhost:3000'],
+    //},
   },
 }
 
